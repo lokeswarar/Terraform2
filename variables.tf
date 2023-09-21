c3d07cf095b97fc90a427776d6b542b5693982ed
lokeswarar <144889290+lokeswarar@users.noreply.github.com>
2023-09-21 12:08:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "instance_ami" {
   default     = "ami-03a6eaae9938c858c"
 }
 
-variable "instance_name" {
+variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
